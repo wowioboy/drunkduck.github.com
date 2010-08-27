0fb15dc8c1e048cb87a0f4d15c67b88f2b698cb8
dan <danielkram@gmail.com>
2010-08-27 11:47:13 -0700

    contact updates

--- contact.inc.php
+++ contact.inc.php
@@ -6,22 +6,12 @@
   <p>General requests:&nbsp;&nbsp; <a href="mailto:Info@DrunkDuck.com">Info@DrunkDuck.com</a></p>
   <br>
 	<p>
-	  <b>WOWIO, Inc.</b><br>
-    11400 W. Olympic Blvd, 14th Floor<br>
-    Los Angeles, CA 90064<br>
-  </p>
-  <p>
-    Phone: (310) 807-8181<br>
-    Fax: (310) 276-2799
+	  <b>WOWIO, Inc.</b>
   </p>
   <hr />
   <h3><a name="about" id="about"></a>About DrunkDuck</h3>
-  <p>DrunkDuck is a website that provides free hosting services for webcomic creators.&nbsp; Founded in 2002 by Dylan Squires, it has evolved into one of the largest online webcomic communities with over 7,000 comic strips and stories.&nbsp; In addition to free hosting, DrunkDuck also provides free message boards and forums for its members, as well as a user rating system that allows for feedback and ranking of its webcomics.&nbsp; </p>
-  <p>In the past four years, DrunkDuck has undergone two data losses and four design revisions.&nbsp; The first loss, in 2003, was caused by a full server failure.&nbsp; In 2005, the site&rsquo;s host shut off their servers due to financial difficulty, losing massive amounts of data in the process.&nbsp; As a testament to the strength of its community, Drunk Duck&rsquo;s creators returned in droves on both occasions.</p>
-  <p>In August of 2006, DrunkDuck was purchased by Platinum Studios, an entertainment company controlling the world&rsquo;s largest independent library of comic characters, and producing comics based entertainment in film, television, internet, mobile, and print.&nbsp; Dylan Squires stayed on with the company becoming the Director of Software Development for Platinum Studios&rsquo; New Media Group. With this new support, DrunkDuck has been able to immediately upgrade to a dedicated server and begin backing up the data for its thousands of creators, preventing the possibility of another crash and improving site performance.&nbsp; It is currently undergoing an ongoing series of design changes and upgrades. These upgrades include new tools for the creators as well as improvement in the community functions.</p>
-  <p><strong><em>Our mission for DrunkDuck is to make the best possible home on the web for webcomics, and for the webcomics community.</em></strong></p>
-  <hr />
-  <h3>Why a duck?</h3>
-  <p>Shortly before Dylan Squires founded DrunkDuck in 2002, his pet duck, Professor Quackers, passed away.&nbsp; One of his friends, designing a logo for Dylan&rsquo;s new site, drew a cartoon of the deceased Professor Quackers as a joke.&nbsp; The logo stuck (for a short while) and Dylan, believing Dead Duck to be a less than appealing company name, decided that the cartoon duck could just as easily be passed out after a night of heavy drinking rather than the more morbid alternative.&nbsp; Thus, DrunkDuck was born.</p>
-  <p>&nbsp;</p>
+  <p>DrunkDuck is a website that provides free hosting services for webcomic creators. Founded in 2002 by Dylan Squires, it has evolved into one of the largest online webcomic communities. In addition to free hosting, DrunkDuck also provides in-depth art, writing, and technical tutorials written by members of the DrunkDuck community, a robust forums section to interact with one another, a user-generated merchandise store for creators to sell their work to their fans, and many other wonderful free features.</p>
+  <p>In 2010, DrunkDuck was acquired by WOWIO Inc., a growing eBook marketplace, and along with its sister site WEvolt.com, the three sites have come to comprise the “WOWIO family”. The goal of the three sites is to compliment each other in such a way as to allow webcomic creators a clear path to growing as an artist and honing the quality of their content, attract an enthusiastic audience and increase the popularity of their work, and eventually translate that work into a revenue stream.</p>
+  <p>DrunkDuck provides the foundation of that plan. We offer many free tools and a strong community to not only help a webcomic creator publish their work online, but also to continually learn and grow as an artist. As the quality of the work improves, so will the quantity of the audience.</p>
+  <i><b>Our mission for DrunkDuck is to make the best possible home on the web for webcomics, and for the webcomics community.</b></i>
 </div>
