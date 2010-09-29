8b8daa17fd307442f32d3fe7c92fe403d5baa21e
Cihan Inam <me@instatik.net>
2010-09-29 16:00:16 -0700

    added the comic baseline code

---
+++ comics/resource_files/comic_page_load.inc.php
@@ -0,0 +1,214 @@
+<?
+define('TEMPLATE_VIEW', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+
+// Make sure the php_self is seen as NOT in comics.
+$_SERVER['PHP_SELF'] = preg_replace('`\/comics\/[a-zA-Z0-9_](.*)`', "\\1", $_SERVER['PHP_SELF']);
+
+if ( strstr($_SERVER['REQUEST_URI'], '/comics') ) {
+  if ( strlen($_SERVER['QUERY_STRING']) > 0 ) {
+    header("Location: ".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
+  }
+  else {
+    header("Location: ".$_SERVER['PHP_SELF']);
+  }
+}
+
+$FOLDER_NAME = basename( getcwd() );
+/*
+ * DEV HACK -jihan
+ */
+$FOLDER_NAME = basename($_SERVER['REDIRECT_QUERY_STRING']);
+$COMIC_NAME  = str_replace('_', ' ', $FOLDER_NAME );
+
+$TITLE = $COMIC_NAME;
+
+if ( file_exists('../../includes/global.inc.php') ) {
+  require_once('../../includes/global.inc.php');
+}
+else if ( file_exists('/var/www/html/drunkduck.com/includes/global.inc.php') ) {
+  require_once('/var/www/html/drunkduck.com/includes/global.inc.php');
+}
+else {
+  require_once('../../../includes/global.inc.php');
+}
+
+if ( $USER->flags & FLAG_FROZEN ) {
+  header("Location: http://".DOMAIN."/");
+  die("Your account has been frozen.");
+}
+/****************************
+*                           *
+* LOAD ALL COMIC DATA HERE! *
+*                           *
+****************************/
+$res = db_query("SELECT * FROM comics WHERE comic_name='".$COMIC_NAME."'");
+$COMIC_ROW = db_fetch_object($res);
+db_free_result($res);
+
+if ( $COMIC_ROW->flags & FLAG_FROZEN ) {
+  echo "<DIV ALIGN='CENTER' STYLE='color:red;'>This comic has been frozen.</DIV>";
+  return;
+}
+
+$doCheck = true;
+if ( $COMIC_ROW->user_id == $USER->user_id ) {
+  $doCheck = false;
+}
+if ( $COMIC_ROW->secondary_author == $USER->user_id ) {
+  $doCheck = false;
+}
+if ( !$USER ) {
+  $doCheck = true;
+}
+
+if ( $doCheck )
+{
+  switch($COMIC_ROW->rating_symbol)
+  {
+    case 'E':
+    case 'T':
+    case 'M':
+    break;
+    case 'A':
+       if ( !$USER || ($USER->age < 18) ) {
+         header("Location: /ratings.php?denied=A");
+       }
+    break;
+  }
+
+}
+
+/***********************************
+*                                  *
+* LOAD ALL OF THE USERS FAVORITES  *
+*                                  *
+***********************************/
+
+if ( $USER )
+{
+  $COMIC_FAVS = array();
+  $IN_FAVORITE   = false;
+  $res = db_query("SELECT comic_id, bookmark_page_id FROM comic_favs WHERE user_id='".$USER->user_id."'");
+  while($row = db_fetch_object($res))
+  {
+    if ( $COMIC_ROW->comic_id == $row->comic_id ) {
+      $IN_FAVORITE = true;
+    }
+    $COMIC_FAVS[$row->comic_id] = $row;
+  }
+  db_free_result($res);
+}
+
+
+
+
+
+
+
+$res = db_query("SELECT * FROM users WHERE user_id='".$COMIC_ROW->user_id."'");
+$OWNER_ROW = db_fetch_object($res);
+db_free_result($res);
+
+
+/*******************************************
+*                                          *
+* DECIDE IF WE ARE VIEWING ARCHIVE OR NOT  *
+*                                          *
+*******************************************/
+
+if ( !isset($_GET['p']) && !isset($_POST['p']) && ($COMIC_ROW->flags & FLAG_USE_HOMEPAGE) )
+{
+  include_once('load_homepage.inc.php');
+}
+else
+{
+  include_once('load_archive.inc.php');
+}
+
+
+
+
+
+
+
+
+// $TEMPLATE should be set now.
+$TEMPLATE = str_replace(".cookie", ".cook1e", $TEMPLATE);
+
+//preg_match_all('`\<\!--\[([a-zA-Z0-9_]*)\]--\>`U', $TEMPLATE, $MATCHES);
+preg_match_all('`\<\!--\[(.*)\]--\>`U', $TEMPLATE, $MATCHES);
+
+$adCount = 0;
+foreach($MATCHES[1] as $idx=>$tag)
+{
+  $args = false;
+
+  if ( strstr($tag, '=') ) {
+    list($tag, $args) = explode('=', $tag);
+  }
+
+  if ( isset($GLOBALS['DDTags'][$tag]) )
+  {
+    $CB = $GLOBALS['DDTags'][$tag]->callback;
+    $TEMPLATE = str_replace($MATCHES[0][$idx], $CB($args), $TEMPLATE);
+  }
+}
+
+// strip out new lines... it tends to help the regex
+$TEMPLATE = str_replace("\n", "", $TEMPLATE);
+// Get the header
+
+  $hdr = include(WWW_ROOT.'/comics/resource_files/comic_caps/comic_header_v2.inc.php');
+//  $hdr = include(WWW_ROOT.'/comics/resource_files/comic_caps/comic_header.inc.php');
+
+// Inject the header
+$TEMPLATE = preg_replace("`<(body.*)>`iUm", "<\\1>".$hdr, $TEMPLATE, 1);
+
+// Get the footer
+$ftr = include(WWW_ROOT.'/comics/resource_files/comic_caps/comic_footer.inc.php');
+// Inject the footer.
+$TEMPLATE = preg_replace("`<(/body.*)>`iUm", $ftr."<\\1>", $TEMPLATE, 1);
+
+
+if ( !strstr($TEMPLATE, '<!--HDR-->') ) {
+  $url = 'http://'.DOMAIN.'/'.comicNameToFolder($COMIC_ROW->comic_name).'/';
+  $TEMPLATE = $hdr.$TEMPLATE;
+}
+
+if ( !strstr($TEMPLATE, '<!--FTR-->') ) {
+  $url = 'http://'.DOMAIN.'/'.comicNameToFolder($COMIC_ROW->comic_name).'/';
+  $TEMPLATE = $TEMPLATE.$ftr;
+}
+
+/*
+if ( strstr($TEMPLATE, 'eval ') ) {
+  $TEMPLATE = str_replace('eval ', 'evil ', $TEMPLATE);
+}
+if ( strstr($TEMPLATE, 'eval(') ) {
+  $TEMPLATE = str_replace('eval(', 'evil(', $TEMPLATE);
+}
+if ( strstr($TEMPLATE, 'fromCharCode') ) {
+  $TEMPLATE = str_replace('fromCharCode', 'fronCharCode', $TEMPLATE);
+}
+*/
+
+// Finally inject META code:
+
+  $META = array();
+  $META[] = '<meta name="robots" content="All,INDEX,FOLLOW">';
+  $META[] = '<meta name="description" content="Drunk Duck is the webcomics community that provides FREE hosting and memberships to people who love to read or write comic books, or comic strips.">';
+  $META[] = '<meta name="keywords" content="The Webcomics Community, Webcomics Community, The Comics Community, Comics Community, Comics, Webcomics, Stories, Strips, Comic Strips, Comic Books, Funny, Interesting, Read, Art, Drawing, Photoshop, '.implode(", ", $PAGE_TAGS).'">';
+  //$TEMPLATE = str_replace('</title>', '</title>'.implode("\n", $META), $TEMPLATE);
+  //$TEMPLATE = str_replace('</TITLE>', '</TITLE>'.implode("\n", $META), $TEMPLATE);
+  $TEMPLATE = preg_replace("`(<title>.*</title>)`iUm", "\\1\n".implode("\n", $META), $TEMPLATE, 1);
+
+
+
+// Track this view as a possible unique.
+db_query("INSERT INTO unique_comic_tracking (comic_id, ymd_date, page_id, ip) VALUES ('".$COMIC_ROW->comic_id."', '".YMD."', '".((int)$PAGE_ROW->page_id)."', '".$_SERVER['REMOTE_ADDR']."')");
+// Track pageview:
+track_comic_view($COMIC_ROW->comic_id);
+
+echo $TEMPLATE;
+?>
---
+++ comics/resource_files/load_archive.inc.php
@@ -0,0 +1,254 @@
+<?
+/****************************
+*                           *
+*    LOAD ALL PAGE DATA!    *
+*                           *
+****************************/
+if ( isset($_GET['p']) ) {
+  $PAGE_ID = (int)$_GET['p'];
+}
+else if ( isset($_POST['p'])) {
+  $PAGE_ID = (int)$_POST['p'];
+}
+else {
+  $PAGE_ID = false;
+}
+
+if ( $PAGE_ID !== false )
+{
+  $res = db_query("SELECT * FROM comic_pages WHERE page_id='".$PAGE_ID."' AND comic_id='".$COMIC_ROW->comic_id."' AND post_date<='".time()."' ORDER BY order_id DESC LIMIT 1");
+  if ($PAGE_ROW = db_fetch_object($res)) {
+    $PAGE_ID = $PAGE_ROW->page_id;
+  }
+  db_free_result($res);
+}
+
+// If no page id was specified
+if ( $PAGE_ID === false )
+{
+  $res = db_query("SELECT * FROM comic_pages WHERE comic_id='".$COMIC_ROW->comic_id."' AND post_date<='".time()."' ORDER BY order_id DESC LIMIT 1");
+  $PAGE_ROW = db_fetch_object($res);
+  db_free_result($res);
+}
+
+
+// If they are on an actual comic page....
+if ( $PAGE_ROW )
+{
+    $PAGE_LIST = array();
+    $res = db_query("SELECT page_id, order_id, post_date, page_title, is_chapter FROM comic_pages WHERE comic_id='".$COMIC_ROW->comic_id."' AND post_date<='".time()."' ORDER BY order_id DESC");
+    while ($row = db_fetch_object($res) ) {
+      $PAGE_LIST[$row->order_id] = $row;
+    }
+    db_free_result($res);
+
+    $res       = db_query("SELECT * FROM users WHERE user_id='".$PAGE_ROW->user_id."'");
+    $OWNER_ROW = db_fetch_object($res);
+    db_free_result($res);
+
+    if ( $USER )
+    {
+      if ( ($USER->user_id == $COMIC_ROW->user_id) || ($USER->user_id == $COMIC_ROW->secondary_author) || ($USER->flags & FLAG_IS_ADMIN) || ($USER->flags & FLAG_IS_MOD) ) {
+        $ALLOW_MODERATION = true;
+      }
+      else {
+        $ALLOW_MODERATION = false;
+      }
+    }
+
+    $PAGE_TAGS = array();
+    $res = db_query("SELECT tag FROM tags_by_page WHERE comic_id='".$COMIC_ROW->comic_id."' AND page_id='".$PAGE_ROW->page_id."' ORDER BY counter DESC LIMIT 10");
+    if ( db_num_rows($res) == 0 ) {
+      db_free_result($res);
+      $res = db_query("SELECT tag FROM tags_by_comic WHERE comic_id='".$COMIC_ROW->comic_id."'");
+    }
+
+    while($row = db_fetch_object($res))
+    {
+      $PAGE_TAGS[] = $row->tag;
+    }
+
+    /***********************************
+    *                                  *
+    *         AUTO BOOKMARK            *
+    *                                  *
+    ***********************************/
+    /*
+    if ( $USER && $PAGE_ROW && isset($COMIC_FAVS[$COMIC_ROW->comic_id]) )
+    {
+      if ( $COMIC_FAVS[$COMIC_ROW->comic_id]->bookmark_page_id != $PAGE_ROW->page_id )
+      {
+        db_query("UPDATE comic_favs SET bookmark_page_id='".$PAGE_ROW->page_id."' WHERE user_id='".$USER->user_id."' AND comic_id='".$COMIC_ROW->comic_id."'");
+      }
+    }
+    */
+
+    /***********************************
+    *                                  *
+    * DONE LOADING ALL IMPORTANT DATA! *
+    *                                  *
+    ***********************************/
+}
+
+if ( !$PAGE_ROW || !$COMIC_ROW->total_pages )
+{
+  echo("This comic has no pages yet!");
+  return;
+}
+
+
+/***********************************
+*                                  *
+*    PROCESS POSTED DATA HERE!     *
+*                                  *
+***********************************/
+
+// Handle page comments, if they are on a comic page.
+if ( $PAGE_ROW )
+{
+    if ( isset($_POST['com2']) && ($USER->flags & FLAG_VERIFIED) )
+    {
+      $rating  = 0;
+      $vWeight = 0;
+      if ( $USER &&
+          ($USER->user_id != $COMIC_ROW->user_id) &&
+          ($USER->user_id != $COMIC_ROW->secondary_author) &&
+          ($USER->ip != $OWNER_ROW->ip) )
+      {
+        $res = db_query("SELECT * FROM page_comments WHERE page_id='".$PAGE_ROW->page_id."' AND (user_id='".$USER->user_id."' OR ip='".$_SERVER['REMOTE_ADDR']."') AND vote_rating>0");
+        if ( db_num_rows($res) == 0 ) {
+          $rating  = $_POST['vote_rating'];
+          $vWeight = $USER->vote_weight;
+        }
+        db_free_result($res);
+      }
+
+      if ( $rating > 5 ) $rating = 5;
+
+      $flags = 0;
+      if ( $_POST['anonymous'] || !$USER ) {
+        $flags |= COMMENT_ANONYMOUS;
+      }
+
+      $comment = strip_tags(trim($_POST['comment']));
+
+      if ( (strlen($comment)==0) && ($rating==0) )
+      {
+        echo "<DIV ALIGN='CENTER' STYLE='FONT-SIZE:14px;COLOR:#FF0000;BACKGROUND:#000000;'><B>You didn't say anything!</B></DIV>";
+      }
+      else
+      {
+        preg_match_all('/[^\[\]\/ ]{50}/', $comment, $FOUND);
+        $FOUND = $FOUND[0];
+        if ( count($FOUND) ) {
+          $FOUND = array_unique($FOUND);
+          foreach($FOUND as $str) {
+            $comment = str_replace($str, $str.' ', $comment);
+          }
+        }
+
+        db_query("INSERT INTO page_comments (comic_id, page_id, user_id, post_date, flags, comment, vote_rating, vote_weight, ip) VALUES ('".$COMIC_ROW->comic_id."', '".$PAGE_ROW->page_id."', '".(int)$USER->user_id."', '".time()."', '".$flags."', '".db_escape_string($comment)."', '".$rating."', '".$vWeight."', '".$_SERVER['REMOTE_ADDR']."')");
+
+        if ( $USER && ($USER->user_id != $COMIC_ROW->user_id) && ($USER->user_id != $COMIC_ROW->secondary_author) )  {
+          add_comment_karma($USER->user_id, 1);
+        }
+
+        recalcPageScore($PAGE_ROW->page_id);
+
+        header("Location: http://".DOMAIN."/".$FOLDER_NAME."/?p=".$PAGE_ROW->page_id);
+      }
+    }
+}
+else // Since there is no page being viewed, this must be the 'home page'
+{
+  // process home page post data
+}
+
+/***********************************
+*                                  *
+*   DONE PROCESSING POSTED DATA    *
+*                                  *
+***********************************/
+
+
+
+
+/***********************************
+*                                  *
+*    PROCESS GET'ED DATA HERE!     *
+*                                  *
+***********************************/
+
+if ( $ALLOW_MODERATION )
+{
+  // if they are viewing a page ( not the home page )
+  if ( $PAGE_ROW )
+  {
+      if ( isset($_GET['mute']) ) {
+        $res = db_query("SELECT * FROM page_comments WHERE comment_id='".(int)$_GET['mute']."' AND comic_id='".$COMIC_ROW->comic_id."'");
+        if ( $row = db_fetch_object($res) ) {
+          db_query("UPDATE page_comments SET flags='".($row->flags | COMMENT_MUTED)."' WHERE comment_id='".(int)$_GET['mute']."'");
+
+          if ( $row->user_id && db_rows_affected()>0 ) {
+            $karma = get_karma_object($row->user_id);
+            $karma->comments_muted++;
+            db_query("UPDATE karma_tracking SET comments_muted='".$karma->comments_muted."' WHERE user_id='".$karma->user_id."'");
+          }
+        }
+        db_free_result($res);
+      }
+      else if ( isset($_GET['unmute']) ) {
+        $res = db_query("SELECT * FROM page_comments WHERE comment_id='".(int)$_GET['unmute']."' AND comic_id='".$COMIC_ROW->comic_id."'");
+        if ( $row = db_fetch_object($res) ) {
+          db_query("UPDATE page_comments SET flags='".($row->flags & ~COMMENT_MUTED)."' WHERE comment_id='".(int)$_GET['unmute']."'");
+
+          if ( $row->user_id && db_rows_affected()>0) {
+            $karma = get_karma_object($row->user_id);
+            $karma->comments_muted--;
+            db_query("UPDATE karma_tracking SET comments_muted='".$karma->comments_muted."' WHERE user_id='".$karma->user_id."'");
+          }
+        }
+        db_free_result($res);
+      }
+      else if ( isset($_GET['report']) ) {
+        $res = db_query("SELECT * FROM page_comments WHERE comment_id='".(int)$_GET['report']."' AND comic_id='".$COMIC_ROW->comic_id."'");
+        if ( $row = db_fetch_object($res) ) {
+          db_query("UPDATE page_comments SET flags='".($row->flags | COMMENT_UNDER_REVIEW)."' WHERE comment_id='".(int)$_GET['report']."'");
+          db_query("INSERT INTO comment_reports ( comment_id, reported_user_id, reported_ip ) VALUES ('".(int)$_GET['report']."', '".$row->user_id."', '".$row->ip."')");
+
+          if ( $row->user_id && db_rows_affected()>0 ) {
+            $karma = get_karma_object($row->user_id);
+            $karma->comments_reported++;
+            db_query("UPDATE karma_tracking SET comments_reported='".$karma->comments_reported."' WHERE user_id='".$karma->user_id."'");
+          }
+        }
+        db_free_result($res);
+      }
+  }
+  else
+  {
+    // process GET data for home page.
+  }
+}
+
+/***********************************
+*                                  *
+*   DONE PROCESSING GET'ED DATA    *
+*                                  *
+***********************************/
+
+require_once(WWW_ROOT.'/comics/resource_files/template_functions/dd_tag_functions.inc.php');
+require_once(WWW_ROOT.'/comics/resource_files/template_functions/dd_tags_archive.inc.php');
+
+
+if ( $COMIC_ROW->template != 0 ) {
+  $TEMPLATE = implode('', file(WWW_ROOT.'/comics/resource_files/templates/'.$COMIC_ROW->template.'/template.dd'));
+}
+else if ( file_exists(WWW_ROOT.'/comics/'.$FOLDER_NAME{0}.'/'.$FOLDER_NAME.'/pages/template.dd') ) {
+  $TEMPLATE = implode('', file(WWW_ROOT.'/comics/'.$FOLDER_NAME{0}.'/'.$FOLDER_NAME.'/pages/template.dd'));
+}
+else {
+    $TEMPLATE = implode('', file(WWW_ROOT.'/comics/resource_files/default_template.new.dd'));
+}
+
+?>
