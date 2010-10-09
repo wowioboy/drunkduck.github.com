e56ef3557f2a0daa5fca6050345eade0d2973463
dan <danielkram@gmail.com>
2010-10-08 18:40:14 -0700

    fixed stuff like old header

--- templating/new_v3/html_header.inc.php
+++ templating/new_v3/html_header.inc.php
@@ -31,7 +31,7 @@
 <META NAME="robots" CONTENT="index,follow">
 <script src="/__utm.js" type="text/javascript"></script>
 <title>DrunkDuck: The Webcomics Community - <?=$TITLE?></title>
-<link href="/gfx/site_gfx_new_v3/ddstyles.css?cache=<?=date("YmdhIs")?>" rel="stylesheet" type="text/css" />
+<link href="http://www.drunkduck.com/gfx/site_gfx_new_v3/ddstyles.css?cache=<?=date("YmdhIs")?>" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?=HTTP_JAVASCRIPT?>/modal/css/modal-message.css?cache=<?=date("Ymd")?>" type="text/css">
 <script type="text/javascript" src="<?=HTTP_JAVASCRIPT?>/prototype-1.4.0_modified.js?cache=<?=date("YmdhIs")?>"></script>
 <script type="text/javascript" src="<?=HTTP_JAVASCRIPT?>/commonJS.js?cache=<?=date("Ymd")?>"></script>
@@ -40,52 +40,36 @@
 <script type="text/javascript" src="<?=HTTP_JAVASCRIPT?>/modal/js/ajax-dynamic-content.js?cache=<?=date("Ymd")?>"></script>
 
 <!-- DAN ADDED THESE -->
-<link rel="stylesheet" href="/css/jquery/cupertino/jquery-ui-1.8.1.custom.css" />
-<link rel="stylesheet" href="/css/uploadify/uploadify.css" />
-<script src="/js/jquery/jquery-1.4.2.min.js"></script>
-<script src="/js/jquery/jquery-ui-1.8.1.custom.min.js"></script>
-<script src="/js/uploadify/swfobject.js"></script>
-<script src="/js/uploadify/jquery.uploadify.js"></script>
+<link rel="stylesheet" href="http://www.drunkduck.com/css/jquery/cupertino/jquery-ui-1.8.1.custom.css" />
+<link rel="stylesheet" href="http://www.drunkduck.com/css/uploadify/uploadify.css" />
+<script src="http://www.drunkduck.com/js/jquery/jquery-1.4.2.min.js"></script>
+<script src="http://www.drunkduck.com/js/jquery/jquery-ui-1.8.1.custom.min.js"></script>
+<script src="http://www.drunkduck.com/js/uploadify/swfobject.js"></script>
+<script src="http://www.drunkduck.com/js/uploadify/jquery.uploadify.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:bold' rel='stylesheet' type="text/css">
 <script>
 // VERY IMPORTANT TO MAKE SURE THAT JQUERY DOESNT SCREW UP CURRENT JS FRAMEWORK
 jQuery.noConflict();
 </script>
 
-<link rel="stylesheet" href="/css/global.css" type="text/css" />
-<link rel="stylesheet" href="/css/layout.css" type="text/css" />
-<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:bold' rel='stylesheet' type="text/css" />
-<style type="text/css">
-.nav_link {
-  font-family:'Yanone Kaffeesatz';
-    font-weight:bold;
-    display:inline;
-    font-size:large;
-    margin-left:10px;
-    margin-right:10px;
-    /* color:'teal';*/
-}
-body {    
-   /*background-color:rgb(9,153,68);
-    background-image:url('/media/images/bg-gradient-color3.jpg');
-    background-repeat: repeat-x;
-    background-position:center top;*/
-    }
-</style>
+
 </head>
 
-<body onLoad="for(var cs=0; cs<commandStack.length;cs++){eval(commandStack[cs]);}" style="margin:0">
+<body onLoad="for(var cs=0; cs<commandStack.length;cs++){eval(commandStack[cs]);}">
+
 
 <!--<script type="text/javascript" src="http://www.platinumstudios.com/processing/ps_hat_top.js"></script>-->
-
+<div style="width:100%;position:absolute;top:0;height:120px;z-index:-2000;background-color:rgb(9,153,68);background-image:url('/media/images/bg-gradient-color3.jpg');background-repeat: repeat-x;background-position:center top;">
 <div align="center">
-<div class="green" style="width:100%;height:130px;">
-  <div style="width:908px;padding:0;margin:0;position:relative;background-color:#afe207;height:50px;">
+
+
+<div class="" style="width:908px;padding:0;margin:0;position:relative">
     <div style="padding:0;margin:0;float:left;width:180px;">
         <a href="/">
             <img src="/media/images/drunkduck-logo.png" width="180" />
         </a>
     </div>
-    <?php include(WWW_ROOT.'/ads/ad_includes/comic_template/728x90_et.html'); ?>
+    
     <style>
 
 #menu {
@@ -118,6 +102,18 @@ body {
 outline:none;
 border:0;
 }
+#menu {
+    position:static;
+    clear:both;
+    }
+#menu a {
+    float:left;
+    margin-bottom:20px;
+    clear:both;
+    padding: 0;
+    width:160px;
+    text-align:right;
+    }
 #advsearch {
     float:right;
     padding-bottom:30px;
@@ -131,9 +127,22 @@ border:0;
     padding-left:10px;
     }   
     </style>
+    <div style="float:left;width:728px;padding:0m;margin:0;">
+<script type="text/javascript">
+  var gr_ads_zone = 'ros_atf';
+  var gr_ads_size = '728x90';
+</script>
+<script type="text/javascript" src="http://a.giantrealm.com/gr.drunkduck/a.js">
+</script>
+<noscript>
+  <a href="http://ans.giantrealm.com/click/gr.drunkduck/ros_atf;tile=1;sz=728x90;ord=1234567890">
+    <img src="http://ans.giantrealm.com/img/gr.drunkduck/ros_atf;tile=1;sz=728x90;ord=1234567890" width="728" height="90" alt="advertisement" />
+  </a>
+</noscript>    </div>
     
-    <div id="menu" style="width:720px;vertical-align:middle;height:20px;position:absolute;top:100px;right:0px;">
+    <div class="menu" style="width:720px;vertical-align:middle;height:20px;position:absolute;top:100px;right:0px;">
           
+            <?php if (!$nosearch) : ?>
             <div style="float:left;display:inline;background-image:url('/media/images/search.jpg');width:154px;height:20px" class="span-16">
              <link type="text/css" rel="stylesheet" href="/css/search.css" />
               <script type="text/javascript" src="/js/search.js"></script>
@@ -148,20 +157,21 @@ border:0;
             <!--<div id="advsearch" style="height:20px;padding-left:5px;line-height:5px;margin-top:-2px"> 
                 <a href="/search.php" style="">Advanced Search</a> 
             </div>--> 
+                <?php endif; ?>
             
             <!-- menu -->
-            <a href="/search.php">comics</a>
-            <a href="/account/overview/add_comic.php">create</a>
-            <a href="/news_v2.php">news</a>
-            <a href="/tutorials/">tutorials</a>
+            <a href="/search.php" style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">comics</a>
+            <a href="/account/overview/add_comic.php" style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">create</a>
+            <a href="/news_v2.php" style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">news</a>
+            <a href="/tutorials/"  style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">tutorials</a>
             <!--<a href="#">videos</a>-->
-            <a href="/community">forums</a>
-            <a href="http://store.drunkduck.com">store</a>
+            <a href="/community" style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">forums</a>
+            <a href="http://store.drunkduck.com"  style="padding: 0 15px 0 15px;text-decoration:none;color:white;font:bold 20px/15px 'Yanone Kaffeesatz';height:10px;float:left;">store</a>
 
         
     </div>
-    </div>
-  </div>
+</div>
+  <!--END header-->
   <table width="1024" height="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
       <td align="center" valign="top">
@@ -211,10 +221,10 @@ border:0;
                     $('favorites').innerHTML      = 'Loading...';
                     $('favorites').style.display  = '';
                     fetch_content('favorites[1]'+qty);
-					document.myFavorites.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
+          document.myFavorites.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
                   }
                   else {
-				  	document.myFavorites.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
+            document.myFavorites.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
                     $('favorites').style.display  = 'none';
                   }
                 }
@@ -226,15 +236,15 @@ border:0;
                     $('my_comics').innerHTML      = 'Loading...';
                     $('my_comics').style.display  = '';
                     fetch_content('my_comics');
-					document.myComics.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
+          document.myComics.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
                   }
                   else {
-				  	document.myComics.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
+            document.myComics.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
                     $('my_comics').style.display  = 'none';
                   }
                 }
 
-		/* function expandNewsUpdate() to change the news update dynamically*/
+    /* function expandNewsUpdate() to change the news update dynamically*/
                 function expandNewsUpdate()
                 {
                   if($('my_news_update').style.display=='none')
@@ -257,10 +267,10 @@ border:0;
                     $('my_news').innerHTML      = 'Loading...';
                     $('my_news').style.display  = '';
                     fetch_content('my_news');
-					document.myComicsNew.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
+          document.myComicsNew.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
                   }
                   else {
-				 	 document.myComicsNew.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
+           document.myComicsNew.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
                     $('my_news').style.display  = 'none';
                   }
                 }
@@ -271,11 +281,11 @@ border:0;
                   {
                     $('ddcam').style.display  = '';
                     $('ddcam').innerHTML      = '<embed src="http://player.stickam.com/stickamPlayer/174625835-2743920" type="application/x-shockwave-flash" width="300" height="300" scale="exactfit" allowScriptAccess="always" allowFullScreen="true"></embed>'+
-                                                '<div align="center"><a href="/community/view_topic.php?tid=39951&cid=226" target="_blank">Discuss in the forum</a></div>';
-					document.DDCam.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
+                                                '<div align="center"><a href="http://www.drunkduck.com/community/view_topic.php?tid=39951&cid=226" target="_blank">Discuss in the forum</a></div>';
+          document.DDCam.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow.gif';
                   }
                   else {
-				  	document.DDCam.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
+            document.DDCam.src='<?=IMAGE_HOST?>/site_gfx_new_v3/expand_arrow2.gif';
                     $('ddcam').style.display  = 'none';
                   }
                 }
@@ -314,4 +324,3 @@ border:0;
 
                 var modalMenu;
               </script>
-
